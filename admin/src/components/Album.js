b1ec12e36623adf0d113afcad2f44d303d02adf3
sylvain <sylvain.laugier.net@gmail.com>
2018-02-05 21:00:40 +0100

    add album to db feature

--- admin/src/components/Album.js
+++ admin/src/components/Album.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 
 const style = {
   width: 'calc(50% - 20px)',
@@ -12,8 +14,47 @@ export default class Album extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      test: '',
+      existInDatabase: false,
+      databaseInfos: {},
     };
+    this.renderActions = this.renderActions.bind(this);
+    this.addAlbumToNeo4J = this.addAlbumToNeo4J.bind(this);
+  }
+  componentDidMount() {
+    return fetch(`albums/${this.props.album.id}`)
+      .then(res => res.json())
+      .then(dbAlbumInfo => this.setState({
+        existInDatabase: !_.isEmpty(dbAlbumInfo),
+        databaseInfos: dbAlbumInfo,
+      }));
+  }
+  addAlbumToNeo4J() {
+    // First we search for album in spotify to get the complete object
+    return fetch(`albums/get-spotify/${this.props.album.id}`)
+      .then(res => res.json())
+      .then((albumSpotifyObject) => {
+        // once the search is done we send the result to add Db
+        fetch('albums/add-album', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(albumSpotifyObject),
+        }).then(res => res.json())
+          .then((insertedAlbum) => {
+            console.log('added :', insertedAlbum);
+            this.setState({
+              existInDatabase: !_.isEmpty(insertedAlbum),
+              databaseInfos: insertedAlbum,
+            });
+          });
+      });
+  }
+  renderActions() {
+    if (this.state.existInDatabase) {
+      return <FlatButton label="Manage" />;
+    }
+    return <FlatButton label="Add to DB" onClick={this.addAlbumToNeo4J} />;
   }
   render() {
     return (
@@ -22,6 +63,9 @@ export default class Album extends Component {
           <img src={this.props.album.images[0].url} alt="" />
         </CardMedia>
         <CardTitle title={this.props.album.name} subtitle={this.props.album.artists[0].name} />
+        <CardActions>
+          {this.renderActions()}
+        </CardActions>
       </Card>
     );
   }
