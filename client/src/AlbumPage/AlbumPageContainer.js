7dd8902704adb26a6c21c2cbf2fc15b738537f8c
sylvain <sylvain.laugier.net@gmail.com>
2018-03-05 21:03:51 +0100

    more styleees

--- client/src/AlbumPage/AlbumPageContainer.js
+++ client/src/AlbumPage/AlbumPageContainer.js
@@ -3,19 +3,51 @@ import PropTypes from 'prop-types';
 
 import SpotifyContainer from './SpotifyContainer';
 
+const fontSizeGenerator = (size) => {
+  if (size <= 10) {
+    return {
+      fontSize: '4rem',
+      top: '4rem',
+    };
+  }
+  const difference = ((size - 10) * 2) / 10;
+  const fontSize = 4 - difference;
+  if (fontSize < 2) {
+    return {
+      fontSize: '2rem',
+      top: '8rem',
+    };
+  }
+  return {
+    fontSize: `${4 - difference}rem`,
+    top: `${4 + difference}rem`,
+  };
+};
+
 export default class AlbumPageContainer extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       loading: true,
+      styleArtist: {
+        fontSize: '4rem',
+        top: '4rem',
+      },
+    };
+  }
+  componentWillReceiveProps(nextProps) {
+    if (typeof nextProps.artist.name !== 'undefined') {
+      this.setState({
+        styleArtist: fontSizeGenerator(nextProps.artist.name.length),
+      });
     }
   }
   render() {
     return (
       <div className="Album-Page-Container">
         <div className="album-content-header-background" >
-          <h2>{`${this.props.album.name} `}<br /> par</h2>
-          <h3>{`${this.props.artist.name}`}</h3>
+          <h2>{`${this.props.album.name} `}<br /> <span>par</span></h2>
+          <h3 style={this.state.styleArtist}>{`${this.props.artist.name}`}</h3>
         </div>
         <div className="album-content-container" >
           <div className="album-cover">
--- client/src/AlbumPage/ChoiceContainer.js
+++ client/src/AlbumPage/ChoiceContainer.js
@@ -3,22 +3,23 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 export default class ChoiceContainer extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       album: {}
-    }
+    };
   }
   render() {
     if (this.props.richChoices.length > 0) {
       return (
         <div className="album-page-choice-container">
-        {this.props.richChoices.map(richChoice => <Link  to={`/${richChoice.targetObj._id}`}>
-          <div className="button button-album-page">
-            <h3>{richChoice.message}</h3>
-          </div>
-        </Link> )}
-      </div>
+          {this.props.richChoices.map(richChoice => (
+            <Link  to={`/${richChoice.targetObj._id}`}>
+              <div className="button button-album-page">
+                <p>{richChoice.message}</p>
+              </div>
+            </Link>))}
+        </div>
       );
     }
     return null;
