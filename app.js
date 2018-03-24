64c593d8060e230772a42952c984d4dbd12bfd9e
sylvain <sylvain.laugier.net@gmail.com>
2018-03-24 17:25:16 +0100

    test

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ var albums = require('./routes/albums');
 var app = express();
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
