9e3f1e209ab5ef4b5ba310e413f1abd6db31cc89
sylvain <sylvain.laugier.net@gmail.com>
2018-03-24 20:29:28 +0100

    move client and admin out of directory

--- client/src/AlbumPage/AlbumPageHeader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const AlbumPageHeader = () => (
-  <div className="Album-Page-Header">
-    <h1>Electronic music for people who don&apos;t like <span>electronic music</span> </h1>
-  </div>
-);
-
-export default AlbumPageHeader;
